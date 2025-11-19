5ca7f8fcc2b8ef7782be3496d57d696b01df8fb4
Bamidev <bamidev@pm.me>
2025-11-19 22:48:19 +0100

    Add nix flake for publishing website

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Publish the website";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        publishSite = pkgs.writers.writeBashBin "publish-site" (with pkgs; ''
+          set -e
+
+          ${hugo}/bin/hugo
+          ${rsync}/bin/rsync -p 12346 ./public stonenet@stonenet.org:/var/www/stonenet.org
+        '');
+      in {
+        apps.publish = {
+          name = "Publish the website";
+          type = "app";
+          program = "${publishSite}/bin/publish-site";
+        };
+      }
+    );
+}
