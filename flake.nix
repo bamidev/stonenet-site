6dd1bf1dbc69c6003ec7c00663860e22bf9fd6ab
Bamidev <bamidev@pm.me>
2025-11-20 09:23:50 +0100

    Add nix run app for running the dv server

--- flake.nix
+++ flake.nix
@@ -6,21 +6,45 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (system: 
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        publishSite = pkgs.writers.writeBashBin "publish-site" (with pkgs; ''
-          set -e
-
-          ${hugo}/bin/hugo
-          ${rsync}/bin/rsync --delete -r -e '${openssh}/bin/ssh -p 12346' ./public/* stonenet@stonenet.org:/var/www/stonenet.org
-        '');
-      in {
-        apps.publish = {
-          name = "Publish the website";
-          type = "app";
-          program = "${publishSite}/bin/publish-site";
+        publishSite = pkgs.writers.writeBashBin "publish-site" (
+          with pkgs;
+          ''
+            set -e
+            ${hugo}/bin/hugo
+            ${rsync}/bin/rsync --delete -r -e '${openssh}/bin/ssh -p 12346' ./public/* stonenet@stonenet.org:/var/www/stonenet.org
+          ''
+        );
+        runDevServer = pkgs.writers.writeBashBin "run-dev-server" (
+          with pkgs;
+          ''
+            set -e
+            ${hugo}/bin/hugo server $@
+          ''
+        );
+      in
+      {
+        apps = {
+          default = {
+            name = "Run development server";
+            type = "app";
+            program = "${runDevServer}/bin/run-dev-server";
+            args = [ "server" ];
+          };
+          publish = {
+            name = "Publish the website";
+            type = "app";
+            program = "${publishSite}/bin/publish-site";
+          };
         };
       }
     );
